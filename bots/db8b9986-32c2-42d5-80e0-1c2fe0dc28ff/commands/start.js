5f040295e49b0a1455ab24eb60a1343ff2fb1366
Anish Aryal <hellesomie@gmail.com>
2026-04-17 15:57:25 +0545

    Sync command /start for bot db8b9986-32c2-42d5-80e0-1c2fe0dc28ff

--- bots/db8b9986-32c2-42d5-80e0-1c2fe0dc28ff/commands/start.js
+++ bots/db8b9986-32c2-42d5-80e0-1c2fe0dc28ff/commands/start.js
@@ -1,1 +1,9 @@
-Api.sendMessage({ text: "Hello!" })
+Bot.sendMessage("🌦 *Advanced Weather Bot*\n\nWelcome " + user.first_name + "! I provide real-time weather updates worldwide.\n\nUse the buttons below to manage your settings or just send me a city name!", {
+  parse_mode: "Markdown",
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: "📍 View My Saved City", callback_data: "/my_city" }],
+      [{ text: "⚙️ User Settings", callback_data: "/settings" }]
+    ]
+  })
+})
