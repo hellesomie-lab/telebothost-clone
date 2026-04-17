439736df19b1604d225c3df5a51f54bee6a3d1cd
Anish Aryal <hellesomie@gmail.com>
2026-04-17 10:21:14 +0545

    Sync command /start for bot 07db2f23-ea5d-41ba-b882-2d712e2f564c

--- bots/07db2f23-ea5d-41ba-b882-2d712e2f564c/commands/start.js
+++ bots/07db2f23-ea5d-41ba-b882-2d712e2f564c/commands/start.js
@@ -1,1 +1,12 @@
-Bot.sendMessage("Hello " + user.first_name + "! Welcome to my bot.")
+// A simple greeting that uses the user's Telegram First Name
+Bot.sendMessage(`Hello there, ${user.first_name}! 👋\n\nWelcome to my amazing new bot!`);
+
+// Let's also send an interactive button just to show off what the platform can do!
+await ctx.reply("Here is a quick menu for you:", {
+  reply_markup: {
+    inline_keyboard: [
+      [{ text: "Visit TeleBotHost Clone", url: "https://telebothost-clone.vercel.app" }],
+      [{ text: "Click Me (Callback)", callback_data: "ping" }]
+    ]
+  }
+});
