9bc7a5f19deab284ee688339298b14c34d3f638d
Anish Aryal <hellesomie@gmail.com>
2026-04-17 12:34:42 +0545

    Sync command /start for bot d443682e-daf5-4367-ae31-988595528d21

--- bots/d443682e-daf5-4367-ae31-988595528d21/commands/start.js
+++ bots/d443682e-daf5-4367-ae31-988595528d21/commands/start.js
@@ -1,1 +1,11 @@
-Api.sendMessage({ text: "Hello user!" })
+Bot.sendMessage(`Hello there, ${user.first_name}! 👋\n\nWelcome to my amazing new bot!`);
+
+// Let's also send an interactive button just to show off what the platform can do!
+await ctx.reply("Here is a quick menu for you:", {
+  reply_markup: {
+    inline_keyboard: [
+      [{ text: "Visit TeleBotHost Clone", url: "https://telebothost-clone.vercel.app" }],
+      [{ text: "Click Me (Callback)", callback_data: "ping" }]
+    ]
+  }
+});
